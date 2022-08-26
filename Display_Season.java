416083789932558d3d064fb2a95f3e20474cb58a
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-26 22:41:51 +0530

    import java.util.Scanner; class multiply {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int c;           c=sc.nextInt();         if(c==4 || c==5 || c==6)         {             System.out.print("Summer");         }         else if(c==7 || c==8 ||c==9 ||c==10)         {             System.out.print("Rainy");         }         else if(c==11 || c==12 || c==1)         {             System.out.print("Winter");         }         else if(c==2 || c==3)         {             System.out.print("Spring");         }         else         {           System.out.print("-1");         }
        }
    }

---
+++ Display_Season.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int c;
+          c=sc.nextInt();
+        if(c==4 || c==5 || c==6)
+        {
+            System.out.print("Summer");
+        }
+        else if(c==7 || c==8 ||c==9 ||c==10)
+        {
+            System.out.print("Rainy");
+        }
+        else if(c==11 || c==12 || c==1)
+        {
+            System.out.print("Winter");
+        }
+        else if(c==2 || c==3)
+        {
+            System.out.print("Spring");
+        }
+        else
+        {
+          System.out.print("-1");  
+        }
+        
+    }
+}
