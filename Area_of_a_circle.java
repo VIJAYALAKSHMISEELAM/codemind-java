22299c78ab52de1dbf5c3fa67a9ace9597fea347
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-26 11:30:16 +0530

    import java.util.Scanner; class area {     public static void main(String args[])     {        Scanner sc=new Scanner(System.in);        int r;        float pi=3.14f;        float a;        r=sc.nextInt();        a=pi*r*r;        System.out.println(a);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class area
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int r;
+       float pi=3.14f;
+       float a;
+       r=sc.nextInt();
+       a=pi*r*r;
+       System.out.println(a);
+       
+    }
+}
