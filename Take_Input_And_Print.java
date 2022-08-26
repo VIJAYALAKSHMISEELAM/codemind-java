1f5decad4fdebe02c3fb3493ae0b69f67bec31fa
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-26 21:13:14 +0530

    import java.util.Scanner; class multiply {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String a;         a=sc.nextLine();          System.out.printf("%s",a);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a;
+        a=sc.nextLine();
+         System.out.printf("%s",a);
+        
+        
+    }
+}
