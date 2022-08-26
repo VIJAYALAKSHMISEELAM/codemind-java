18a515cb72ba6a288013c3f82b2fc845195492b6
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-26 21:23:25 +0530

    import java.util.Scanner; class multiply {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();           b=a*(a-1)/2;          System.out.println(b);
        }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+          b=a*(a-1)/2;
+         System.out.println(b);
+        
+        
+    }
+}
