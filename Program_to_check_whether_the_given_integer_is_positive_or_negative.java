7cd76110fdae26ecdc66ef54dd196f44abfee000
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-08-27 20:47:51 +0530

    import java.util.Scanner; class multiply {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int c;           c=sc.nextInt();         if(c<0)         {             System.out.println("Negative Number");         }         else         {
             System.out.println("Positive Number");
            }
            
        }
    }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int c;
+          c=sc.nextInt();
+        if(c<0)
+        {
+            System.out.println("Negative Number");
+        }
+        else
+        {
+            
+         System.out.println("Positive Number");
+        }
+        
+    }
+}
