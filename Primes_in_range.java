55e121ac487cd3405a92bff6b85d6458ec6879bd
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-09-09 11:11:38 +0530

    import java.util.Scanner; class primes {     static int prime(int i)     {         int j;         for(j=2;j<=Math.sqrt(i);j++)         {             if(i%j==0)             {                 return 0;             }         }         return 1;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,i,c;         a=sc.nextInt();         b=sc.nextInt();         c=0;
            for(i=a;i<=b;i++)
            {
                if(i==1)
                {
                    continue;
                }
                else if(prime(i)==1)
                {
                    c+=1;
                }
            }
            System.out.println(c);
        }
    }

---
+++ Primes_in_range.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class primes
+{
+    static int prime(int i)
+    {
+        int j;
+        for(j=2;j<=Math.sqrt(i);j++)
+        {
+            if(i%j==0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,i,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=0;
+    
+        for(i=a;i<=b;i++)
+        {
+            if(i==1)
+            {
+                continue;
+            }
+            else if(prime(i)==1)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
