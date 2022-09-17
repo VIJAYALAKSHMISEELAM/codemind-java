5320523d8506b20630f81241757ab416fb053343
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-09-17 20:06:56 +0530

    import java.util.Scanner; class minimum {
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,i,c=0;
            
            int []arr= new int[100];
            n=sc.nextInt();
            
              for(i=0;i<n;i++)
              {
                  arr[i]=sc.nextInt();
                  
              }
             for(i=0;i<n;i++)
              {
                  if(arr[i]%2!=0)
                  {
                      c=1;
                       System.out.print("False");
                      break;
                  }
        
              }
            
            if(c==0)
            {
                System.out.print("True");
            }
        }
    }

---
+++ Even_Array.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class minimum
+{
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
+        
+        int []arr= new int[100];
+        n=sc.nextInt();
+        
+          for(i=0;i<n;i++)
+          {
+              arr[i]=sc.nextInt();
+              
+          }
+         for(i=0;i<n;i++)
+          {
+              if(arr[i]%2!=0)
+              {
+                  c=1;
+                   System.out.print("False");
+                  break;
+              }
+    
+          }
+        
+        if(c==0)
+        {
+            System.out.print("True");
+        }
+    }
+}
