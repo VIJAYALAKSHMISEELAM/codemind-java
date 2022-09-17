b431ab52cbad90b5b7918e602532abd353c17f76
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-09-17 15:48:36 +0530

    import java.util.Scanner; class minimum {
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,i,a,b,sum=0;
            float avg;
            int []arr= new int[100];
            int []temp= new int[100];
            int []res= new int[100];
            n=sc.nextInt();
            
              for(i=0;i<n;i++)
              {
                  arr[i]=sc.nextInt();
              }
              
             for(i=0;i<n;i++)
              {
                  sum+=arr[i];
              }
            avg=(float)sum/n;
          System.out.printf("%.2f",avg);
        }
    }

---
+++ average_of_the_array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class minimum
+{
+    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,a,b,sum=0;
+        float avg;
+        int []arr= new int[100];
+        int []temp= new int[100];
+        int []res= new int[100];
+        n=sc.nextInt();
+        
+          for(i=0;i<n;i++)
+          {
+              arr[i]=sc.nextInt();
+          }
+          
+         for(i=0;i<n;i++)
+          {
+              sum+=arr[i];
+          }
+        avg=(float)sum/n;
+      System.out.printf("%.2f",avg);
+    }
+}
