cac8d37be31e66b04153f10ee458c47fbf471e29
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-10-02 21:38:27 +0530

    import java.util.Scanner; class near {     public static boolean is_it_prime(int n)     {         int j;         for(j=2;j<=Math.sqrt(n);j++)         {             if(n%j==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,temp,c=0,d=0;         n=sc.nextInt();         temp=n;         while(true)         {
                if(n==1)
                {
                    continue;
                }
                else if(is_it_prime(n))
                {
                    c=Math.abs(temp-n);
                    break;
                }
                n+=1;
            }
            n=temp;
            while(true)
            {
               
                if(n==1)
                {
                    continue;
                }
                else if(is_it_prime(n))
                {
                    d=Math.abs(temp-n);
                    break;
                }
                 n-=1;
            }
            if(c<=d)
            {
                System.out.println(c);
            }
            else
            {
                System.out.println(d);
            }
        }
    }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+class near
+{
+    public static boolean is_it_prime(int n)
+    {
+        int j;
+        for(j=2;j<=Math.sqrt(n);j++)
+        {
+            if(n%j==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,temp,c=0,d=0;
+        n=sc.nextInt();
+        temp=n;
+        while(true)
+        {
+            
+            if(n==1)
+            {
+                continue;
+            }
+            else if(is_it_prime(n))
+            {
+                c=Math.abs(temp-n);
+                break;
+            }
+            n+=1;
+        }
+        n=temp;
+        while(true)
+        {
+           
+            if(n==1)
+            {
+                continue;
+            }
+            else if(is_it_prime(n))
+            {
+                d=Math.abs(temp-n);
+                break;
+            }
+             n-=1;
+        }
+        if(c<=d)
+        {
+            System.out.println(c);
+        }
+        else
+        {
+            System.out.println(d);
+        }
+    }
+}
