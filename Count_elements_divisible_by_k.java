238d853e4e6b2aeba5ca68686251225c8b8f5970
VIJAYALAKSHMISEELAM <98726762+VIJAYALAKSHMISEELAM@users.noreply.github.com>
2022-11-25 21:06:11 +0530

    import java.util.Scanner; class minimum {
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,i,c=0,k;
            int []arr= new int[100];
            n=sc.nextInt();
            k=sc.nextInt();
            
              for(i=0;i<n;i++)
              {
                  arr[i]=sc.nextInt();
              }
              
             for(i=0;i<n;i++)
              {
                  if(arr[i]%k==0)
                  {
                      c+=1;
                  }
              }
             System.out.println(c);
        }
    }

--- Count_elements_divisible_by_k.java
+++ Count_elements_divisible_by_k.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
-class divisible
+class minimum
 {
+    
     public static void main(String args[])
     {
-        int n,k,i,c;
-        c=0;
         Scanner sc=new Scanner(System.in);
+        int n,i,c=0,k;
+        int []arr= new int[100];
         n=sc.nextInt();
         k=sc.nextInt();
-        int []a;
-        a=new int[100];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        for(i=0;i<n;i++)
-        {
-            if(a[i]%k==0)
-            {
-                c+=1;
-            }
-            
-        }
-        System.out.println(c);
         
+          for(i=0;i<n;i++)
+          {
+              arr[i]=sc.nextInt();
+          }
+          
+         for(i=0;i<n;i++)
+          {
+              if(arr[i]%k==0)
+              {
+                  c+=1;
+              }
+          }
+         System.out.println(c);
     }
 }
